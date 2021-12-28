f53f8e6bc5de2870356f7e01b1644b2f9139df15
Jamesford <jamie@stobbs.me>
2021-12-28 19:22:00 +0100

    Update command order, add readme

---
+++ readme.md
@@ -0,0 +1,21 @@
+# rname
+
+```
+❯ rname -h
+Usage: rname [options] [command]
+
+Rename TV and Movies for Plex, with the help of TMDb
+
+Options:
+  -v, --version   output the version number
+  -d, --debug     display logs (default: false)
+  -h, --help      display help for command
+
+Commands:
+  movie|m         rename a movie
+  tv|t            rename a tv show
+  setup           setup rname before using
+  test            reset rname test directory
+  help [command]  display help for command
+
+```
--- src/main.js
+++ src/main.js
@@ -7,20 +7,20 @@ const program = new Command();
 program
   .name("rname")
   .version("0.1.0", "-v, --version")
-  .description("Rename plex media")
+  .description("Rename TV and Movies for Plex, with the help of TMDb")
   .option("-d, --debug", "display logs", false)
-  .command("setup", "setup rname before using", {
-    executableFile: "cmd/setup",
-  })
-  .command("tv", "rename a tv show", {
-    executableFile: "cmd/tv",
-  })
-  .alias("t")
   .command("movie", "rename a movie", {
     executableFile: "cmd/movie",
   })
   .alias("m")
-  .command("test", "setup rname test directory", {
+  .command("tv", "rename a tv show", {
+    executableFile: "cmd/tv",
+  })
+  .alias("t")
+  .command("setup", "setup rname before using", {
+    executableFile: "cmd/setup",
+  })
+  .command("test", "reset rname test directory", {
     executableFile: "cmd/test",
   });
 
