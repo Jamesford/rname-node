ca708cf0420da02ae71443ff585177ee7a37c066
Jamesford <jamie@stobbs.me>
2021-12-28 11:37:52 +0100

    Update test command to create dummy media directory

--- src/cmd/test.js
+++ src/cmd/test.js
@@ -1,14 +1,63 @@
 import { Command } from "commander";
 import { errors } from "../lib/errors.js";
-import { getConfig, writeConfig, updateConfig } from "../lib/config.js";
+import { exec as execute } from "child_process";
+import path from "path";
+import fs from "fs/promises";
 
-const program = new Command()
-  .option("-f, --force", "force an action", false)
-  .action(errors(main))
-  .parse();
+const program = new Command().action(errors(main)).parse();
 
-async function main(options, cmd) {
-  const opts = cmd.opts();
-  console.log({ opts });
-  console.log(await getConfig());
+async function main(_options, _cmd) {
+  const tvFiles = [
+    "RARBG.txt",
+    "RARBG_DO_NOT_MIRROR.exe",
+    "Star.Wars.The.Bad.Batch.S01E09.720p.WEB.h264-KOGi.mkv",
+    "Star.Wars.The.Bad.Batch.S01E01.720p.WEB.h264-KOGi.mkv",
+    "Star.Wars.The.Bad.Batch.S01E10.720p.WEB.H264-EXPLOIT.mkv",
+    "Star.Wars.The.Bad.Batch.S01E02.720p.WEB.h264-KOGi.mkv",
+    "Star.Wars.The.Bad.Batch.S01E11.720p.WEB.H264-EXPLOIT.mkv",
+    "Star.Wars.The.Bad.Batch.S01E03.720p.WEB.h264-KOGi.mkv",
+    "Star.Wars.The.Bad.Batch.S01E12.720p.WEB.H264-EXPLOIT.mkv",
+    "Star.Wars.The.Bad.Batch.S01E04.720p.WEB.h264-KOGi.mkv",
+    "Star.Wars.The.Bad.Batch.S01E13.720p.WEB.H264-EXPLOIT.mkv",
+    "Star.Wars.The.Bad.Batch.S01E05.720p.WEB.h264-KOGi.mkv",
+    "Star.Wars.The.Bad.Batch.S01E14.720p.WEB.H264-EXPLOIT.mkv",
+    "Star.Wars.The.Bad.Batch.S01E06.720p.WEB.h264-KOGi.mkv",
+    "Star.Wars.The.Bad.Batch.S01E15.720p.WEB.H264-EMPATHY.mkv",
+    "Star.Wars.The.Bad.Batch.S01E07.720p.WEB.h264-KOGi.mkv",
+    "Star.Wars.The.Bad.Batch.S01E16.720p.WEB.H264-EMPATHY.mkv",
+    "Star.Wars.The.Bad.Batch.S01E08.720p.WEB.h264-KOGi.mkv",
+  ];
+
+  const dir = process.cwd();
+  if (await exists(path.join(dir, "media"))) {
+    await fs.rm(path.join(dir, "media"), { recursive: true, force: true });
+  }
+  await fs.mkdir(path.join(dir, "media"));
+  await fs.mkdir(path.join(dir, "media", "tv"));
+
+  const tvPromises = tvFiles.map((file) =>
+    exec(`touch ${path.join(dir, "media", "tv", file)}`)
+  );
+  const results = await Promise.allSettled(tvPromises);
+  results.forEach(({ status, reason }) => {
+    if (status === "rejected") console.error(reason);
+  });
+}
+
+async function exec(cmd, { ignore, ...options } = { ignore: false }) {
+  return new Promise((resolve, reject) => {
+    execute(cmd, options, (err, stdout, stderr) => {
+      if (err && !ignore) return reject(err);
+      return resolve([stdout, stderr]);
+    });
+  });
+}
+
+async function exists(path) {
+  try {
+    await fs.stat(path);
+    return true;
+  } catch (_err) {
+    return false;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -9,16 +9,16 @@ program
   .version("0.1.0", "-v, --version")
   .description("Rename plex media")
   .option("-d, --debug", "display logs", false)
-  //   .command("test", "test command", {
-  //     executableFile: "cmd/test",
-  //   })
   .command("setup", "setup rname before using", {
     executableFile: "cmd/setup",
   })
   .command("tv", "rename a tv show", {
     executableFile: "cmd/tv",
   })
-  .alias("t");
+  .alias("t")
+  .command("test", "setup rname test directory", {
+    executableFile: "cmd/test",
+  });
 //   .command("movie", "rename a movie", {
 //     executableFile: "cmd/movie",
 //   })
